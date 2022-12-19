ee97affae7f8cf44987d48cc099b6da9f9347e01
shawn@tradev-group.com <shawn@tradev-group.com>
2022-12-19 15:35:40 +0800

    feat：tauri event

--- src/tauri.d.ts
+++ src/tauri.d.ts
@@ -1,6 +1,7 @@
 import type * as dialog from '@tauri-apps/api/dialog'
 import type * as fs from '@tauri-apps/api/fs'
 import type * as path from '@tauri-apps/api/path'
+import type * as event from '@tauri-apps/api/event'
 
 declare global {
   interface Window {
@@ -8,6 +9,7 @@ declare global {
       fs?: typeof fs
       path?: typeof path
       dialog?: typeof dialog
+      event?: typeof event
     }
   }
 }
